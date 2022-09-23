0cc6ff17494212876df098b7a981f157218ccac9
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-09-23 14:20:15 -0300

    Add files via upload

---
+++ assets/preload/stages/onsen.lua
@@ -0,0 +1,18 @@
+function onCreate()
+
+	makeLuaSprite('onsenbg','onsen',-200,-300)
+	addLuaSprite('onsenbg',false)
+
+end
+
+function onBeatHit( ... )--for every beat
+	-- body
+end
+
+function onStepHit( ... )--for every step
+	-- body
+end
+
+function onUpdate( ... )
+	-- body
+end
